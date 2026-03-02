5d80bcdd57ef7ee95b1d2405e9869ed2d5f1f348
Koichi Imai <koichi.imai.2@tier4.jp>
2026-03-02 11:17:33 +0900

    fix clang-format
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- agnocast_kmod/agnocast_kunit/agnocast_kunit_bridge_shutdown.h
+++ agnocast_kmod/agnocast_kunit/agnocast_kunit_bridge_shutdown.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <kunit/test.h>
 
-#define TEST_CASES_BRIDGE_SHUTDOWN                               \
-  KUNIT_CASE(test_case_bridge_manager_flag_set_on_registration), \
+#define TEST_CASES_BRIDGE_SHUTDOWN                                \
+  KUNIT_CASE(test_case_bridge_manager_flag_set_on_registration),  \
     KUNIT_CASE(test_case_bridge_manager_detected_by_new_process), \
     KUNIT_CASE(test_case_notify_bridge_shutdown_clears_flag)
 
