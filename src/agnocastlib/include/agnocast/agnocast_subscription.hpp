autowarefoundation/agnocast

commit ee3c4e43835ac71c8768de5ff53a098f1a2fc057

feat(agnocastlib): feat tracepoints agnocast node (#1093)
* feat: agnocast_node_init

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* feat: TODO TORACEPOINT

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* feat: agnocast::Node TRACEPOINT

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* feat: insert agnocast::Node TRACEPOINT

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* revert: not suppoer service/client

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* revert: not suppoer service/client

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* refactor: agnocast_node

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* refactor: add name in agnocast_add_callback_group

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* fix: minor

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* refactor: use timer_handle

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* feat: insert add_callback_group

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* refactor: remove debug

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* refactor: period

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* refactor: CMakeList

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* refactor: dummy callback group

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* refactor: timer

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* refactor: timer

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* refactor: all path test

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* refactor: minor

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* refactor: lowercase

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* test: ros2

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* refactor: merge

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* test: old

Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

* refactor: count

src/agnocastlib/include/agnocast/agnocast_subscription.hpp
@@ -33,6 +33,11 @@ extern std::mutex mmap_mtx;
 
 void map_read_only_area(const pid_t pid, const uint64_t shm_addr, const uint64_t shm_size);
 
+// Create a dummy callback group for agnocast::Node tracepoint use.
+// Defined in .cpp to avoid circular inclusion between agnocast_subscription.hpp and
+// agnocast_node.hpp.
+rclcpp::CallbackGroup::SharedPtr create_dummy_callback_group(agnocast::Node * node);
+
 struct SubscriptionOptions
 {
   rclcpp::CallbackGroup::SharedPtr callback_group{nullptr};
@@ -168,10 +173,17 @@ public:
   {
     rclcpp::CallbackGroup::SharedPtr callback_group = get_valid_callback_group(node, options);
 
-    [[maybe_unused]] const rclcpp::QoS actual_qos =
+    const rclcpp::QoS actual_qos =
       constructor_impl(node, qos, std::forward<Func>(callback), callback_group, options, false);
 
-    // TODO(atsushi421): CARET tracepoint for agnocast::Node
+    {
+      uint64_t pid_callback_info_id = (static_cast<uint64_t>(getpid()) << 32) | callback_info_id_;
+      TRACEPOINT(
+        agnocast_subscription_init, static_cast<const void *>(this),
+        static_cast<const void *>(node), static_cast<const void *>(&callback),
+        static_cast<const void *>(callback_group.get()), tracetools::get_symbol(callback),
+        topic_name_.c_str(), actual_qos.depth(), pid_callback_info_id);
+    }
   }
 
   ~BasicSubscription()
@@ -251,9 +263,18 @@ public:
     agnocast::SubscriptionOptions options = agnocast::SubscriptionOptions())
   : SubscriptionBase(node, topic_name)
   {
-    constructor_impl(node, qos, options);
+    const rclcpp::QoS actual_qos = constructor_impl(node, qos, options);
 
-    // TODO(atsushi421): CARET tracepoint for agnocast::Node
+    {
+      auto dummy_cbg = create_dummy_callback_group(node);
+      auto dummy_cb = []() {};
+      std::string dummy_cb_symbols = "dummy_take" + topic_name;
+      TRACEPOINT(
+        agnocast_subscription_init, static_cast<const void *>(this),
+        static_cast<const void *>(node), static_cast<const void *>(&dummy_cb),
+        static_cast<const void *>(dummy_cbg.get()), dummy_cb_symbols.c_str(), topic_name_.c_str(),
+        actual_qos.depth(), 0);
+    }
   }
 
   agnocast::ipc_shared_ptr<const MessageT> take(bool allow_same_message = false)

src/agnocastlib/include/agnocast/agnocast_tracepoint_call.h
@@ -18,6 +18,32 @@
 
 TRACEPOINT_EVENT(
   TRACEPOINT_PROVIDER,
+  agnocast_init,
+  TP_ARGS(
+    const void *, context_handle_arg
+  ),
+  TP_FIELDS(
+    ctf_integer_hex(const void *, context_handle, context_handle_arg)
+  )
+)
+
+TRACEPOINT_EVENT(
+  TRACEPOINT_PROVIDER,
+  agnocast_node_init,
+  TP_ARGS(
+    const void *, node_handle_arg,
+    const char *, node_name_arg,
+    const char *, namespace_arg
+  ),
+  TP_FIELDS(
+    ctf_integer_hex(const void *, node_handle, node_handle_arg)
+    ctf_string(node_name, node_name_arg)
+    ctf_string(namespace, namespace_arg)
+  )
+)
+
+TRACEPOINT_EVENT(
+  TRACEPOINT_PROVIDER,
   agnocast_publisher_init,
   TP_ARGS(
     const void *, publisher_handle_arg,
@@ -60,6 +86,42 @@ TRACEPOINT_EVENT(
 
 TRACEPOINT_EVENT(
   TRACEPOINT_PROVIDER,
+  agnocast_timer_init,
+  TP_ARGS(
+    const void *, timer_handle_arg,      
+    const void *, node_handle_arg,      
+    const void *, callback_arg,          
+    const void *, callback_group_arg,    
+    const char *, symbol_arg,            
+    int64_t, period_arg                     
+  ),
+  TP_FIELDS(
+    ctf_integer_hex(const void *, timer_handle, timer_handle_arg)
+    ctf_integer_hex(const void *, node_handle, node_handle_arg)
+    ctf_integer_hex(const void *, callback, callback_arg)
+    ctf_integer_hex(const void *, callback_group, callback_group_arg)
+    ctf_string(symbol, symbol_arg)
+    ctf_integer(const int64_t, period, period_arg)
+  )
+)
+
+TRACEPOINT_EVENT(
+  TRACEPOINT_PROVIDER,
+  agnocast_add_callback_group,
+  TP_ARGS(
+    const void *, executor_addr_arg,
+    const void *, callback_group_addr_arg,
+    const char *, group_type_name_arg
+  ),
+  TP_FIELDS(
+    ctf_integer_hex(const void *, executor_addr, executor_addr_arg)
+    ctf_integer_hex(const void *, callback_group_addr, callback_group_addr_arg)
+    ctf_string(group_type_name, group_type_name_arg)
+  )
+)
+
+TRACEPOINT_EVENT(
+  TRACEPOINT_PROVIDER,
   agnocast_publish,
   TP_ARGS(
     const void *, publisher_handle_arg,
@@ -83,6 +145,19 @@ TRACEPOINT_EVENT(
     ctf_integer_hex(const void *, callable, callable_arg)
     ctf_integer(const int64_t, entry_id, entry_id_arg)
     ctf_integer(const uint64_t, pid_callback_info_id, pid_callback_info_id_arg)
+  )
+)
+
+TRACEPOINT_EVENT(
+  TRACEPOINT_PROVIDER,
+  agnocast_create_timer_callable,
+  TP_ARGS(
+    const void *, callable_arg,         
+    const void *, timer_handle_arg      
+  ),
+  TP_FIELDS(
+    ctf_integer_hex(const void *, callable, callable_arg)
+    ctf_integer_hex(const void *, timer_handle, timer_handle_arg)
   )
 )
 

src/agnocastlib/include/agnocast/node/agnocast_node.hpp
@@ -350,6 +350,11 @@ public:
 
     register_timer_info(timer_id, timer, period, group, timer->get_clock());
 
+    TRACEPOINT(
+      agnocast_timer_init, static_cast<const void *>(timer.get()), static_cast<const void *>(this),
+      static_cast<const void *>(&callback), static_cast<const void *>(group.get()),
+      tracetools::get_symbol(callback), period.count());
+
     return timer;
   }
 
@@ -381,6 +386,11 @@ public:
       timer_id, period_ns, clock, std::forward<CallbackT>(callback));
 
     register_timer_info(timer_id, timer, period_ns, group, clock);
+
+    TRACEPOINT(
+      agnocast_timer_init, static_cast<const void *>(timer.get()), static_cast<const void *>(this),
+      static_cast<const void *>(&callback), static_cast<const void *>(group.get()),
+      tracetools::get_symbol(callback), period_ns.count());
 
     return timer;
   }

src/agnocastlib/src/agnocast_subscription.cpp
@@ -1,4 +1,5 @@
 #include "agnocast/agnocast.hpp"
+#include "agnocast/node/agnocast_node.hpp"
 
 namespace agnocast
 {
@@ -78,6 +79,12 @@ void remove_mq(const std::pair<mqd_t, std::string> & mq_subscription)
       logger,
       "mq_unlink failed for mq_name='" << mq_subscription.second << "': " << strerror(errno));
   }
+}
+
+rclcpp::CallbackGroup::SharedPtr create_dummy_callback_group(agnocast::Node * node)
+{
+  return node->get_node_base_interface()->create_callback_group(
+    rclcpp::CallbackGroupType::MutuallyExclusive, false);
 }
 
 }  // namespace agnocast

src/agnocastlib/src/node/agnocast_context.cpp
@@ -1,5 +1,7 @@
 #include "agnocast/node/agnocast_context.hpp"
 
+#include "agnocast/agnocast_tracepoint_wrapper.h"
+
 namespace agnocast
 {
 
@@ -27,6 +29,8 @@ void init(int argc, char const * const * argv)
 {
   std::lock_guard<std::mutex> lock(g_context_mtx);
   g_context.init(argc, argv);
+
+  TRACEPOINT(agnocast_init, static_cast<const void *>(&g_context));
 }
 
 }  // namespace agnocast

src/agnocastlib/include/agnocast/agnocast_publisher.hpp
@@ -134,9 +134,11 @@ public:
     agnocast::Node * node, const std::string & topic_name, const rclcpp::QoS & qos,
     const PublisherOptions & options = PublisherOptions{})
   {
-    constructor_impl(node, topic_name, qos, options, false);
+    const rclcpp::QoS actual_qos = constructor_impl(node, topic_name, qos, options, false);
 
-    // TODO: CARET tracepoint for agnocast::Node
+    TRACEPOINT(
+      agnocast_publisher_init, static_cast<const void *>(this), static_cast<const void *>(node),
+      topic_name_.c_str(), actual_qos.depth());
   }
 
   ~BasicPublisher()

src/agnocastlib/include/agnocast/agnocast_tracepoint_wrapper.h
@@ -13,6 +13,16 @@ extern "C" {
 // clang-format off
 
 DECLARE_TRACEPOINT(
+  agnocast_init,
+  const void * context_handle)
+
+DECLARE_TRACEPOINT(
+  agnocast_node_init,
+  const void * node_handle,
+  const char * node_name,
+  const char * namespace_arg)
+
+DECLARE_TRACEPOINT(
   agnocast_publisher_init,
   const void * publisher_handle,
   const void * node_handle,
@@ -31,6 +41,21 @@ DECLARE_TRACEPOINT(
   const uint64_t pid_callback_info_id)
 
 DECLARE_TRACEPOINT(
+  agnocast_timer_init,
+  const void * timer_handle,
+  const void * node_handle,
+  const void * callback,
+  const void * callback_group,
+  const char * function_symbol,
+  int64_t period)
+
+DECLARE_TRACEPOINT(
+  agnocast_add_callback_group,
+  const void * executor_addr,
+  const void * callback_group_addr,
+  const char * group_type_name)
+
+DECLARE_TRACEPOINT(
   agnocast_publish,
   const void * publisher_handle,
   const int64_t entry_id)
@@ -40,6 +65,11 @@ DECLARE_TRACEPOINT(
   const void * callable,
   const int64_t entry_id,
   const uint64_t pid_callback_info_id)
+
+DECLARE_TRACEPOINT(
+  agnocast_create_timer_callable,
+  const void * callable,
+  const void * timer_handle)
 
 DECLARE_TRACEPOINT(
   agnocast_callable_start,

src/agnocastlib/src/agnocast_epoll.cpp
@@ -45,6 +45,7 @@ bool wait_and_handle_epoll_event(
     // Timer event (timerfd fired)
     const uint32_t timer_id = event_id & ~TIMER_EVENT_FLAG;
     rclcpp::CallbackGroup::SharedPtr callback_group;
+    std::shared_ptr<agnocast::TimerBase> timer_ptr;
 
     std::shared_ptr<TimerInfo> timer_info;
     {
@@ -56,7 +57,8 @@ bool wait_and_handle_epoll_event(
         exit(EXIT_FAILURE);
       }
       timer_info = it->second;
-      if (!timer_info->timer.lock()) {
+      timer_ptr = timer_info->timer.lock();
+      if (!timer_ptr) {
         return false;  // Timer object has been destroyed
       }
       callback_group = timer_info->callback_group;
@@ -79,7 +81,15 @@ bool wait_and_handle_epoll_event(
     // For tracepoints.
     const void * callable_ptr = callable.get();
     // Create a callable that handles the timer event
-    *callable = [timer_info, callable_ptr]() { handle_timer_event(*timer_info); };
+    *callable = [timer_info, callable_ptr]() {
+      TRACEPOINT(agnocast_callable_start, callable_ptr);
+      handle_timer_event(*timer_info);
+      TRACEPOINT(agnocast_callable_end, callable_ptr);
+    };
+
+    TRACEPOINT(
+      agnocast_create_timer_callable, static_cast<const void *>(callable_ptr),
+      static_cast<const void *>(timer_ptr.get()));
 
     {
       std::lock_guard<std::mutex> ready_lock{ready_agnocast_executables_mutex};
@@ -89,6 +99,7 @@ bool wait_and_handle_epoll_event(
     // Clock event (ROS_TIME clock updated via time jump callback)
     const uint32_t timer_id = event_id & ~CLOCK_EVENT_FLAG;
     rclcpp::CallbackGroup::SharedPtr callback_group;
+    std::shared_ptr<agnocast::TimerBase> timer_ptr;
 
     std::shared_ptr<TimerInfo> timer_info;
     {
@@ -100,7 +111,8 @@ bool wait_and_handle_epoll_event(
         exit(EXIT_FAILURE);
       }
       timer_info = it->second;
-      if (!timer_info->timer.lock()) {
+      timer_ptr = timer_info->timer.lock();
+      if (!timer_ptr) {
         return false;  // Timer object has been destroyed
       }
       callback_group = timer_info->callback_group;
@@ -120,8 +132,18 @@ bool wait_and_handle_epoll_event(
     }
 
     // Create a callable that handles the clock event
-    const std::shared_ptr<std::function<void()>> callable =
-      std::make_shared<std::function<void()>>([timer_info]() { handle_timer_event(*timer_info); });
+    auto callable = std::make_shared<std::function<void()>>();
+    const void * callable_ptr = callable.get();
+
+    *callable = [timer_info, callable_ptr]() {
+      TRACEPOINT(agnocast_callable_start, callable_ptr);
+      handle_timer_event(*timer_info);
+      TRACEPOINT(agnocast_callable_end, callable_ptr);
+    };
+
+    TRACEPOINT(
+      agnocast_create_timer_callable, static_cast<const void *>(callable_ptr),
+      static_cast<const void *>(timer_ptr.get()));
 
     {
       std::lock_guard<std::mutex> ready_lock{ready_agnocast_executables_mutex};

commit 3d82e111d85ef73d7fc9dcfed6a17f6a42ae3670

fix(agnocastlib): fix tracepoint bugs introduced in PR #1093
- Fix period unit inconsistency in create_wall_timer (use nanoseconds)
- Capture callback address/symbol before std::forward to avoid use-after-move
- Use default callback group instead of creating dummy ones to prevent accumulation
- Emit agnocast_init only on first initialization to avoid duplicate events
- Remove trailing whitespace in tracepoint definitions
- Use resolved topic name in dummy take subscription symbols

Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

src/agnocastlib/include/agnocast/agnocast_subscription.hpp
@@ -33,10 +33,10 @@ extern std::mutex mmap_mtx;
 
 void map_read_only_area(const pid_t pid, const uint64_t shm_addr, const uint64_t shm_size);
 
-// Create a dummy callback group for agnocast::Node tracepoint use.
+// Get the default callback group from an agnocast::Node for tracepoint use.
 // Defined in .cpp to avoid circular inclusion between agnocast_subscription.hpp and
 // agnocast_node.hpp.
-rclcpp::CallbackGroup::SharedPtr create_dummy_callback_group(agnocast::Node * node);
+rclcpp::CallbackGroup::SharedPtr get_default_callback_group_for_tracepoint(agnocast::Node * node);
 const void * get_node_base_address(Node * node);
 
 struct SubscriptionOptions
@@ -155,6 +155,9 @@ public:
   {
     rclcpp::CallbackGroup::SharedPtr callback_group = get_valid_callback_group(node, options);
 
+    const void * callback_addr = static_cast<const void *>(&callback);
+    const char * callback_symbol = tracetools::get_symbol(callback);
+
     const rclcpp::QoS actual_qos =
       constructor_impl(node, qos, std::forward<Func>(callback), callback_group, options, is_bridge);
 
@@ -164,9 +167,8 @@ public:
         agnocast_subscription_init, static_cast<const void *>(this),
         static_cast<const void *>(
           node->get_node_base_interface()->get_shared_rcl_node_handle().get()),
-        static_cast<const void *>(&callback), static_cast<const void *>(callback_group.get()),
-        tracetools::get_symbol(callback), topic_name_.c_str(), actual_qos.depth(),
-        pid_callback_info_id);
+        callback_addr, static_cast<const void *>(callback_group.get()), callback_symbol,
+        topic_name_.c_str(), actual_qos.depth(), pid_callback_info_id);
     }
   }
 
@@ -178,6 +180,9 @@ public:
   {
     rclcpp::CallbackGroup::SharedPtr callback_group = get_valid_callback_group(node, options);
 
+    const void * callback_addr = static_cast<const void *>(&callback);
+    const char * callback_symbol = tracetools::get_symbol(callback);
+
     const rclcpp::QoS actual_qos =
       constructor_impl(node, qos, std::forward<Func>(callback), callback_group, options, false);
 
@@ -185,10 +190,9 @@ public:
       uint64_t pid_callback_info_id = (static_cast<uint64_t>(getpid()) << 32) | callback_info_id_;
       TRACEPOINT(
         agnocast_subscription_init, static_cast<const void *>(this),
-        static_cast<const void *>(get_node_base_address(node)),
-        static_cast<const void *>(&callback), static_cast<const void *>(callback_group.get()),
-        tracetools::get_symbol(callback), topic_name_.c_str(), actual_qos.depth(),
-        pid_callback_info_id);
+        static_cast<const void *>(get_node_base_address(node)), callback_addr,
+        static_cast<const void *>(callback_group.get()), callback_symbol, topic_name_.c_str(),
+        actual_qos.depth(), pid_callback_info_id);
     }
   }
 
@@ -251,15 +255,14 @@ public:
     const rclcpp::QoS actual_qos = constructor_impl(node, qos, options);
 
     {
-      auto dummy_cbg = node->get_node_base_interface()->create_callback_group(
-        rclcpp::CallbackGroupType::MutuallyExclusive, false);
+      auto default_cbg = node->get_node_base_interface()->get_default_callback_group();
       auto dummy_cb = []() {};
-      std::string dummy_cb_symbols = "dummy_take" + topic_name;
+      std::string dummy_cb_symbols = "dummy_take" + topic_name_;
       TRACEPOINT(
         agnocast_subscription_init, static_cast<const void *>(this),
         static_cast<const void *>(
           node->get_node_base_interface()->get_shared_rcl_node_handle().get()),
-        static_cast<const void *>(&dummy_cb), static_cast<const void *>(dummy_cbg.get()),
+        static_cast<const void *>(&dummy_cb), static_cast<const void *>(default_cbg.get()),
         dummy_cb_symbols.c_str(), topic_name_.c_str(), actual_qos.depth(), 0);
     }
   }
@@ -272,13 +275,13 @@ public:
     const rclcpp::QoS actual_qos = constructor_impl(node, qos, options);
 
     {
-      auto dummy_cbg = create_dummy_callback_group(node);
+      auto default_cbg = get_default_callback_group_for_tracepoint(node);
       auto dummy_cb = []() {};
-      std::string dummy_cb_symbols = "dummy_take" + topic_name;
+      std::string dummy_cb_symbols = "dummy_take" + topic_name_;
       TRACEPOINT(
         agnocast_subscription_init, static_cast<const void *>(this),
         static_cast<const void *>(get_node_base_address(node)),
-        static_cast<const void *>(&dummy_cb), static_cast<const void *>(dummy_cbg.get()),
+        static_cast<const void *>(&dummy_cb), static_cast<const void *>(default_cbg.get()),
         dummy_cb_symbols.c_str(), topic_name_.c_str(), actual_qos.depth(), 0);
     }
   }

src/agnocastlib/include/agnocast/agnocast_tracepoint_call.h
@@ -88,12 +88,12 @@ TRACEPOINT_EVENT(
   TRACEPOINT_PROVIDER,
   agnocast_timer_init,
   TP_ARGS(
-    const void *, timer_handle_arg,      
-    const void *, node_handle_arg,      
-    const void *, callback_arg,          
-    const void *, callback_group_arg,    
-    const char *, symbol_arg,            
-    int64_t, period_arg                     
+    const void *, timer_handle_arg,
+    const void *, node_handle_arg,
+    const void *, callback_arg,
+    const void *, callback_group_arg,
+    const char *, symbol_arg,
+    int64_t, period_arg
   ),
   TP_FIELDS(
     ctf_integer_hex(const void *, timer_handle, timer_handle_arg)
@@ -154,8 +154,8 @@ TRACEPOINT_EVENT(
   TRACEPOINT_PROVIDER,
   agnocast_create_timer_callable,
   TP_ARGS(
-    const void *, callable_arg,         
-    const void *, timer_handle_arg      
+    const void *, callable_arg,
+    const void *, timer_handle_arg
   ),
   TP_FIELDS(
     ctf_integer_hex(const void *, callable, callable_arg)

src/agnocastlib/include/agnocast/node/agnocast_node.hpp
@@ -372,7 +372,7 @@ public:
     TRACEPOINT(
       agnocast_timer_init, static_cast<const void *>(timer.get()),
       static_cast<const void *>(node_base_.get()), callback_addr,
-      static_cast<const void *>(group.get()), callback_symbol, period.count());
+      static_cast<const void *>(group.get()), callback_symbol, period_ns.count());
 
     return timer;
   }
@@ -435,6 +435,9 @@ private:
     const uint32_t timer_id = allocate_timer_id();
     const auto period_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(period);
 
+    const void * callback_addr = static_cast<const void *>(&callback);
+    const char * callback_symbol = tracetools::get_symbol(callback);
+
     auto timer = std::make_shared<GenericTimer<CallbackT>>(
       timer_id, period_ns, clock, std::forward<CallbackT>(callback));
 
@@ -442,8 +445,8 @@ private:
 
     TRACEPOINT(
       agnocast_timer_init, static_cast<const void *>(timer.get()),
-      static_cast<const void *>(node_base_.get()), static_cast<const void *>(&callback),
-      static_cast<const void *>(group.get()), tracetools::get_symbol(callback), period_ns.count());
+      static_cast<const void *>(node_base_.get()), callback_addr,
+      static_cast<const void *>(group.get()), callback_symbol, period_ns.count());
 
     return timer;
   }

src/agnocastlib/src/agnocast_subscription.cpp
@@ -106,10 +106,9 @@ void remove_mq(const std::pair<mqd_t, std::string> & mq_subscription)
   }
 }
 
-rclcpp::CallbackGroup::SharedPtr create_dummy_callback_group(agnocast::Node * node)
+rclcpp::CallbackGroup::SharedPtr get_default_callback_group_for_tracepoint(agnocast::Node * node)
 {
-  return node->get_node_base_interface()->create_callback_group(
-    rclcpp::CallbackGroupType::MutuallyExclusive, false);
+  return node->get_node_base_interface()->get_default_callback_group();
 }
 
 }  // namespace agnocast

src/agnocastlib/src/node/agnocast_context.cpp
@@ -23,14 +23,14 @@ void Context::init(int argc, char const * const * argv)
 
   parsed_arguments_ = parse_arguments(args);
   initialized_ = true;
+
+  TRACEPOINT(agnocast_init, static_cast<const void *>(this));
 }
 
 void init(int argc, char const * const * argv)
 {
   std::lock_guard<std::mutex> lock(g_context_mtx);
   g_context.init(argc, argv);
-
-  TRACEPOINT(agnocast_init, static_cast<const void *>(&g_context));
 }
 
 }  // namespace agnocast