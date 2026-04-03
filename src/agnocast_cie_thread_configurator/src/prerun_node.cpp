1f6252c145629d11f0df8d730ce7c432d2a538a9
atsushi421 <atsushi.yano.2@tier4.jp>
2026-04-03 11:06:03 +0900

    refactor(cie_thread_configurator): simplify NonRosThreadInfo QoS initialization
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

--- src/agnocast_cie_thread_configurator/src/prerun_node.cpp
+++ src/agnocast_cie_thread_configurator/src/prerun_node.cpp
@@ -18,10 +18,7 @@ PrerunNode::PrerunNode(const std::set<size_t> & domain_ids) : Node("prerun_node"
   auto cbg_qos = rclcpp::QoS(rclcpp::QoSInitialization(RMW_QOS_POLICY_HISTORY_KEEP_LAST, 5000))
                    .reliable()
                    .transient_local();
-  auto non_ros_thread_qos =
-    rclcpp::QoS(rclcpp::QoSInitialization(RMW_QOS_POLICY_HISTORY_KEEP_LAST, 5000))
-      .keep_all()
-      .reliable();
+  auto non_ros_thread_qos = rclcpp::QoS(rclcpp::KeepAll()).reliable();
 
   // Create subscription for non-ROS thread info
   non_ros_thread_sub_ = this->create_subscription<agnocast_cie_config_msgs::msg::NonRosThreadInfo>(
--- src/agnocast_cie_thread_configurator/src/thread_configurator_node.cpp
+++ src/agnocast_cie_thread_configurator/src/thread_configurator_node.cpp
@@ -106,10 +106,7 @@ ThreadConfiguratorNode::ThreadConfiguratorNode(const YAML::Node & yaml)
   auto cbg_qos = rclcpp::QoS(rclcpp::QoSInitialization(RMW_QOS_POLICY_HISTORY_KEEP_LAST, 5000))
                    .reliable()
                    .transient_local();
-  auto non_ros_thread_qos =
-    rclcpp::QoS(rclcpp::QoSInitialization(RMW_QOS_POLICY_HISTORY_KEEP_LAST, 5000))
-      .keep_all()
-      .reliable();
+  auto non_ros_thread_qos = rclcpp::QoS(rclcpp::KeepAll()).reliable();
 
   // Create subscription for non-ROS thread info
   non_ros_thread_sub_ = this->create_subscription<agnocast_cie_config_msgs::msg::NonRosThreadInfo>(
