054c3830e764d7642e7a5af37ec2e1e22a93983c
atsushi421 <atsushi.yano.2@tier4.jp>
2026-04-07 16:58:16 +0900

    refactor(agnocast_components): remove unused package_hook.cmake
    The file contained only comments and was not referenced anywhere.
    
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

--- src/agnocast_components/cmake/agnocast_components_package_hook.cmake
+++
@@ -1,3 +0,0 @@
-# Resource index registration is handled by rclcpp_components_package_hook.
-# agnocast_components_register_node writes to the same directory properties
-# as rclcpp_components_register_node, so no additional registration is needed.
