dd403357b0422c2f153d86d1184a1d1575c63e8d
atsushi421 <atsushi.yano.2@tier4.jp>
2026-04-07 16:37:14 +0900

    fix(agnocast_components): allow agnocast_components_register_node to coexist with rclcpp_components_register_node
    Use rclcpp_components directory properties for resource index
    registration instead of separate agnocast-specific variables. This
    allows both macros to be used in the same package without conflicting
    file(GENERATE) calls on the same ament index marker file.
    
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

--- src/agnocast_components/cmake/agnocast_components_package_hook.cmake
+++ src/agnocast_components/cmake/agnocast_components_package_hook.cmake
@@ -1,5 +1,3 @@
-list(REMOVE_DUPLICATES _AGNOCAST_COMPONENTS_PACKAGE_RESOURCE_INDICES)
-foreach(resource_index ${_AGNOCAST_COMPONENTS_PACKAGE_RESOURCE_INDICES})
-  ament_index_register_resource(
-    ${resource_index} CONTENT "${_AGNOCAST_COMPONENTS_${resource_index}__NODES}")
-endforeach()
+# Resource index registration is handled by rclcpp_components_package_hook.
+# agnocast_components_register_node writes to the same directory properties
+# as rclcpp_components_register_node, so no additional registration is needed.
