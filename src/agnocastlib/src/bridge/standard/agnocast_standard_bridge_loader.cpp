3df11f50d0b9daa085ccf3938cb68c79ba8eea92
bdm-k <kokusyunn@gmail.com>
2026-04-28 20:00:46 +0900

    Remove dead code
    Signed-off-by: bdm-k <kokusyunn@gmail.com>

--- src/agnocastlib/src/bridge/standard/agnocast_standard_bridge_loader.cpp
+++ src/agnocastlib/src/bridge/standard/agnocast_standard_bridge_loader.cpp
@@ -148,9 +148,6 @@ std::pair<uintptr_t, std::shared_ptr<void>> StandardBridgeLoader::resolve_factor
   auto [raw_handle, base_addr] = load_library(factory_spec.shared_lib_path);
 
   if (raw_handle == nullptr) {
-    if (raw_handle != nullptr) {
-      dlclose(raw_handle);
-    }
     return {0, nullptr};
   }
 
