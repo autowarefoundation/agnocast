581d76d65027cf0601c292faedb63663ce3fb104
Koichi Imai <koichi.imai.2@tier4.jp>
2026-04-30 14:54:17 +0900

    fix to align with rclcpp
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- src/agnocastlib/include/agnocast/node/diagnostic_updater/diagnostic_updater.hpp
+++ src/agnocastlib/include/agnocast/node/diagnostic_updater/diagnostic_updater.hpp
@@ -1,7 +1,40 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
 #ifndef AGNOCAST__NODE__DIAGNOSTIC_UPDATER__DIAGNOSTIC_UPDATER_HPP_
 #define AGNOCAST__NODE__DIAGNOSTIC_UPDATER__DIAGNOSTIC_UPDATER_HPP_
 
-#include "agnocast/agnocast.hpp"
 #include "agnocast/node/agnocast_node.hpp"
 #include "diagnostic_updater/diagnostic_updater.hpp"
 
@@ -14,31 +47,68 @@
 namespace agnocast
 {
 
-/// \brief agnocast::Node-compatible replacement for diagnostic_updater::Updater.
-///
-/// Reuses DiagnosticTaskVector, DiagnosticTask, DiagnosticStatusWrapper, etc. from
-/// the original diagnostic_updater package. Only the publisher and timer are replaced
-/// with Agnocast equivalents for zero-copy shared memory transport.
+/**
+ * \brief agnocast::Node-compatible replacement for diagnostic_updater::Updater.
+ *
+ * Reuses DiagnosticTaskVector, DiagnosticTask, DiagnosticStatusWrapper, etc. from
+ * the original diagnostic_updater package. Only the publisher and timer are replaced
+ * with Agnocast equivalents for zero-copy shared memory transport.
+ *
+ * The publication rate is determined by the `diagnostic_updater.period` ros2 parameter.
+ * The force_update function can always be triggered async to the period interval.
+ */
 class Updater : public diagnostic_updater::DiagnosticTaskVector
 {
 public:
   bool verbose_;
 
-  /// \brief Constructs an updater.
-  ///
-  /// \param node Reference to an agnocast::Node
-  /// \param period Update period in seconds
+  /**
+   * \brief Constructs an updater.
+   *
+   * \param node Reference to an agnocast::Node.
+   * \param period Update period in seconds. Ignored if `diagnostic_updater.period`
+   *   has already been declared on \p node.
+   */
   explicit Updater(agnocast::Node & node, double period = 1.0);
 
+  /**
+   * \brief Returns the interval between updates.
+   */
   auto getPeriod() const { return period_; }
 
+  /**
+   * \brief Sets the period as a rclcpp::Duration.
+   */
   void setPeriod(rclcpp::Duration period);
+
+  /**
+   * \brief Sets the period given a value in seconds.
+   */
   void setPeriod(double period);
 
+  /**
+   * \brief Forces to send out an update for all known DiagnosticStatus.
+   */
   void force_update();
+
+  /**
+   * \brief Output a message on all the known DiagnosticStatus.
+   *
+   * Useful if something drastic is happening such as shutdown or a self-test.
+   *
+   * \param lvl Level of the diagnostic being output.
+   * \param msg Status message to output.
+   */
   void broadcast(unsigned char lvl, const std::string msg);
 
+  /**
+   * \brief Sets the hardware ID using a printf-style format string.
+   */
   void setHardwareIDf(const char * format, ...);
+
+  /**
+   * \brief Sets the hardware ID.
+   */
   void setHardwareID(const std::string & hwid) { hwid_ = hwid; }
 
 private:
---
+++ src/agnocastlib/test/unit/test_diagnostic_updater.cpp
@@ -0,0 +1,143 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#include "agnocast/node/agnocast_node.hpp"
+#include "agnocast/node/diagnostic_updater/diagnostic_updater_all.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <string>
+
+class DiagnosticUpdaterTest : public ::testing::Test
+{
+protected:
+  void SetUp() override
+  {
+    rclcpp::NodeOptions options;
+    options.start_parameter_services(false);
+    node_ = std::make_shared<agnocast::Node>("test_diagnostic_updater_node", options);
+  }
+
+  void TearDown() override { node_.reset(); }
+
+  std::shared_ptr<agnocast::Node> node_;
+};
+
+TEST_F(DiagnosticUpdaterTest, constructs_with_default_period)
+{
+  agnocast::Updater updater(*node_);
+
+  // Default period is 1.0 second.
+  EXPECT_DOUBLE_EQ(updater.getPeriod().seconds(), 1.0);
+}
+
+TEST_F(DiagnosticUpdaterTest, constructor_declares_period_parameter)
+{
+  agnocast::Updater updater(*node_, 0.5);
+
+  ASSERT_TRUE(node_->has_parameter("diagnostic_updater.period"));
+  EXPECT_DOUBLE_EQ(
+    node_->get_parameter("diagnostic_updater.period").get_parameter_value().get<double>(), 0.5);
+}
+
+TEST_F(DiagnosticUpdaterTest, pre_declared_period_parameter_takes_precedence)
+{
+  node_->declare_parameter<double>("diagnostic_updater.period", 2.5);
+
+  agnocast::Updater updater(*node_, 0.1);
+
+  EXPECT_DOUBLE_EQ(updater.getPeriod().seconds(), 2.5);
+}
+
+TEST_F(DiagnosticUpdaterTest, set_period_updates_period_value)
+{
+  agnocast::Updater updater(*node_);
+  updater.setPeriod(0.25);
+  EXPECT_DOUBLE_EQ(updater.getPeriod().seconds(), 0.25);
+
+  updater.setPeriod(rclcpp::Duration::from_seconds(3.0));
+  EXPECT_DOUBLE_EQ(updater.getPeriod().seconds(), 3.0);
+}
+
+TEST_F(DiagnosticUpdaterTest, set_hardware_id_stores_value)
+{
+  agnocast::Updater updater(*node_);
+  updater.setHardwareID("test-hwid");
+  // No public getter; setHardwareIDf overload exercised separately.
+  // This test ensures the call compiles and does not throw.
+  SUCCEED();
+}
+
+TEST_F(DiagnosticUpdaterTest, set_hardware_idf_formats_string)
+{
+  agnocast::Updater updater(*node_);
+  updater.setHardwareIDf("dev-%d-%s", 42, "agnocast");
+  SUCCEED();
+}
+
+TEST_F(DiagnosticUpdaterTest, add_task_does_not_throw)
+{
+  agnocast::Updater updater(*node_);
+  updater.setHardwareID("none");
+
+  bool task_called = false;
+  updater.add("test-task", [&task_called](diagnostic_updater::DiagnosticStatusWrapper & stat) {
+    task_called = true;
+    stat.summary(diagnostic_msgs::msg::DiagnosticStatus::OK, "ok");
+  });
+
+  // force_update runs all registered tasks and publishes.
+  updater.force_update();
+  EXPECT_TRUE(task_called);
+}
+
+TEST_F(DiagnosticUpdaterTest, broadcast_visits_all_tasks)
+{
+  agnocast::Updater updater(*node_);
+  updater.setHardwareID("none");
+
+  int task_count = 0;
+  updater.add("a", [&](diagnostic_updater::DiagnosticStatusWrapper &) { task_count++; });
+  updater.add("b", [&](diagnostic_updater::DiagnosticStatusWrapper &) { task_count++; });
+
+  // broadcast does not invoke task callbacks; it only emits the supplied summary.
+  // So task_count should remain 0 here.
+  task_count = 0;
+  updater.broadcast(
+    diagnostic_msgs::msg::DiagnosticStatus::WARN, "broadcast-msg");
+  EXPECT_EQ(task_count, 0);
+}
