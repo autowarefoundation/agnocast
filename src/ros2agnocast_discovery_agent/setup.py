97996f2a428d7186e65725a413a7ce5c58b59072
Koichi Imai <koichi.imai.2@tier4.jp>
2026-05-26 13:41:44 +0900

    add pytest for test_agent
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- src/ros2agnocast_discovery_agent/setup.py
+++ src/ros2agnocast_discovery_agent/setup.py
@@ -15,6 +15,7 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
+    tests_require=['pytest'],
     maintainer='Keita Morisaki, Takahiro Ishikawa-Aso, Koichi Imai, Takumi Jin',
     maintainer_email=(
         'keita.morisaki@tier4.jp, sykwer@gmail.com, '
