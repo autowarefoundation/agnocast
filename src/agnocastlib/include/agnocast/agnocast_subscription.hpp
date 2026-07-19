1d5f6223305a59adc73159825a5ae67c741ee4b9
Koichi <koichi.imai.2@tier4.jp>
2026-07-20 07:27:21 +0900

    add tests and comments, and AGNOCAST_PUBLIC
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>

--- src/agnocastlib/include/agnocast/agnocast_subscription.hpp
+++ src/agnocastlib/include/agnocast/agnocast_subscription.hpp
@@ -121,6 +121,11 @@ public:
   SubscriptionBase(rclcpp::Node * node, const std::string & topic_name);
   SubscriptionBase(agnocast::Node * node, const std::string & topic_name);
 
+  /**
+   * @brief Return the fully-resolved topic name.
+   * @return Null-terminated topic name string.
+   */
+  AGNOCAST_PUBLIC
   const char * get_topic_name() const { return topic_name_.c_str(); }
 
   uint32_t get_publisher_count() const { return get_publisher_count_core(topic_name_); }
