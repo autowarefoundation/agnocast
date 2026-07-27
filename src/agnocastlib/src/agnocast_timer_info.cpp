2ec56820a226a17f4dceea2a1091ad2bfb9e701f
tomiy <58660268+tomiy-0x62@users.noreply.github.com>
2026-07-27 11:17:21 +0900

    Implement on_reset_callback for agnocast Timer (#1320)
    * imple cancel() adn reset() for TimerBase
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * imple time_until_trigger() for TimerBase
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * refactor: remove unused code
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * add timer_fd check to TimerBase::time_until_trigger()
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * remove dead return
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * change type of TimerBase::canceled_ to atomic<bool>
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * change TimerBase to hold weak_ptr<TimerInfo> instead of timer_fd
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * add cancellation check before writing to clock_eventfd in TimerInfo::handle_post_time_jump()
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * refactor: move POSIX headers from public header to source file
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * fix: add shared lock when accessing timer_fd in reset()
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * fix: correct exception message construction and errno reporting
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * add unit test for timer api: cancel, reset, time_until_trigger
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * add on_reset_callback
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * remove unused header include
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * add test for on_reset_callback
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * restrict set_timer_info access in TimerBase via friend
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * add description to some method of agnocast::TimerBase
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * wrap on_reset_callback in error handler
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * add reset_counter
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    
    * update the signature of set_timer_info
    
    Signed-off-by: Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
    
    * move member functions of TimerBase to the source file
    
    Signed-off-by: Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
    
    * change the signature of set_on_reset_callback()
    
    The equivalent API in rclcpp takes `std::function<void(size_t)>`
    by value, so we should do the same to align with it. This signature is
    more efficient when the callback is passed as an rvalue.
    
    Signed-off-by: Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
    
    * mention to the exception in the documentation of set_on_reset_callback
    
    Signed-off-by: Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
    
    * update comment of the newly added member function of TimerBase
    
    Signed-off-by: Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
    
    * rename reset_counter to reset_counter_
    
    Signed-off-by: Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
    
    * change the lock range of TimerBase::callback_mutex_
    
    Signed-off-by: Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
    
    * add unit tests for timer reset callback
    
    Signed-off-by: Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
    
    * use agnocast logger when an exception is caught in on_reset_callback
    
    Signed-off-by: Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
    
    * fix copilot review
    
    Signed-off-by: Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
    
    ---------
    
    Signed-off-by: tomiy <tomiy@tomiylab.com>
    Signed-off-by: Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
    Co-authored-by: Takumi Jin <87105992+ruth561@users.noreply.github.com>
    Co-authored-by: Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
    Co-authored-by: Koichi Imai <45482193+Koichi98@users.noreply.github.com>
    Signed-off-by: bdm-k <kokusyunn@gmail.com>

--- src/agnocastlib/src/agnocast_timer_info.cpp
+++ src/agnocastlib/src/agnocast_timer_info.cpp
@@ -236,6 +236,11 @@ uint32_t allocate_timer_id()
   return timer_id;
 }
 
+void set_timer_info(TimerBase & timer, std::weak_ptr<TimerInfo> timer_info)
+{
+  timer.timer_info_ = std::move(timer_info);
+}
+
 void register_timer_info(
   uint32_t timer_id, const std::shared_ptr<TimerBase> & timer, std::chrono::nanoseconds period,
   const rclcpp::CallbackGroup::SharedPtr & callback_group, const rclcpp::Clock::SharedPtr & clock)
@@ -272,7 +277,7 @@ void register_timer_info(
     timer_info->timer_fd = create_timer_fd(timer_id, period, clock->get_clock_type());
   }
 
-  timer->set_timer_info(timer_info);
+  set_timer_info(*timer, timer_info);
 
   setup_time_jump_callback(timer_info, clock);
 
